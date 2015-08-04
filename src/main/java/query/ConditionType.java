fa28d9276c21090e65f89be650b94298ce646b8c
Manoj <manoj.khanwalkar@gmail.com>
2015-08-04 17:46:38 -0400

    init

---
+++ src/main/java/query/ConditionType.java
@@ -0,0 +1,9 @@
+package query;
+
+/**
+ * Created by mkhanwalkar on 8/4/15.
+ */
+public enum ConditionType {
+
+    GT,LT,EQ,IN
+}
--- src/test/java/QuickTester1.java
+++ src/test/java/QuickTester1.java
@@ -1,3 +1,5 @@
+import query.ConditionType;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,7 +13,7 @@ public class QuickTester1 {
     static final String dataBaseName = "demo";
     static final String tableName = "person";
 
-    static int numRecords=-1;
+    static int numRecords=100; //TODO - this value needs to be updated in the metadata .
 
     static int size = 2;
 
@@ -19,25 +21,35 @@ public class QuickTester1 {
 
     public static void main(String[] args) {
 
-        StringBuilder data = readFile(rootDirName+seperator+clusterName+seperator+dataBaseName+seperator+tableName+seperator+"age");
-       // System.out.println(data);
-       // System.out.println(numRecords);
-
         positions = new int[numRecords];
         for (int i=0;i<positions.length;i++)
         {
             positions[i] = 1;
         }
+        checkCondition("age", "25", ConditionType.GT);
+        size = 7;
+        checkCondition("income", "700000", ConditionType.LT);
+        size = 1;
+        checkCondition("gender", "F", ConditionType.EQ);
+        printMatchCount();
+
+    }
+
+    private static void checkCondition(String columnName, String rhs, ConditionType type)
+    {
+        StringBuilder data = readFile(rootDirName+seperator+clusterName+seperator+dataBaseName+seperator+tableName+seperator+columnName);
+
         int count = 0;
         for (int i=0;i<data.length();i+=size)
         {
             String s = data.substring(i,i+size);
-            checkCondition(s, 100, count++);
-       //     System.out.println(s);
+            checkCondition(s, rhs, type , count++);
+            //     System.out.println(s);
         }
-        printMatchCount();
 
     }
+
+
 
     private static void printMatchCount() {
         int count = 0;
@@ -53,19 +65,62 @@ public class QuickTester1 {
 
     }
 
-    private static void checkCondition(String s, int rhs , int count) {
+    private static void checkCondition(String s, String s1 , ConditionType type,  int count) {
 
      //   System.out.println(s);
+        if (positions[count]==0)
+            return ;
 
-        int lhs = Integer.parseInt(s.trim());
-        if (lhs > rhs)
+        switch (type)
         {
-            positions[count] = 1;
+            case GT :
+            {
+                int lhs = Integer.parseInt(s.trim());
+                int rhs = Integer.parseInt(s1.trim());
+                if (lhs > rhs)
+                {
+                    positions[count] = 1;
+                }
+                else
+                {
+                    positions[count] = 0;
+                }
+                break;
+
+            }
+            case LT :
+            {
+                int lhs = Integer.parseInt(s.trim());
+                int rhs = Integer.parseInt(s1.trim());
+                if (lhs < rhs)
+                {
+                    positions[count] = 1;
+                }
+                else
+                {
+                    positions[count] = 0;
+                }
+                break;
+
+            }
+            case EQ :
+            {
+                if (s.trim().equals(s1))
+                {
+                    positions[count] = 1;
+                }
+                else
+                {
+                    positions[count] = 0;
+                }
+                break;
+
+            }
+
+
+
         }
-        else
-        {
-            positions[count] = 0;
-        }
+
     }
 
     private static StringBuilder readFile(String name) {
@@ -74,7 +129,6 @@ public class QuickTester1 {
             FileReader reader = new FileReader(name);
             BufferedReader metaFileReader = new BufferedReader(reader);
             String s=  metaFileReader.readLine();
-            numRecords = s.length()/size;
             metaFileReader.close();
             return new StringBuilder(s);
         } catch (IOException e) {
