56907e1c2b2510aaa254b8dcc60ea075e6cabd13
Manoj <manoj.khanwalkar@gmail.com>
2020-11-09 09:43:50 -0500

    upgrading for Java 11

--- src/test/java/ClientTester.java
+++ src/test/java/ClientTester.java
@@ -12,36 +12,6 @@ public class ClientTester {
         String clusterName = "cluster1";
         String clusterName1 = "cluster2";
 
- /*       for (int j=0;j<1;j++) {
-
-            Thread t = new Thread(()-> {
-
-
-            for (int i = 0; i < 1; i++) {
-                GraphDBClient client = GraphDBClient.getInstance();
-                Request request = new Request();
-                request.setId("DP5");
-                request.setOperation(DBOperation.Query);
-                request.setDbName("db2");
-
-                //  ObjectMapper mapper = new ObjectMapper();
-
-                // String s = mapper.writeValueAsString(request);
-
-                //System.out.println(s);
-
-//
-                Response response = client.send(request);
-
-                System.out.println(response);
-            }
-            });
-
-            t.start();
-
-        }*/
-
-        // GraphDB db = ((DBService) Server.getService("DBService")).getDatabase("db1");
         ColumnDBClient client = ColumnDBClient.getInstance();
 
         client.addCluster(clusterName,10005);
@@ -49,70 +19,7 @@ public class ClientTester {
         client.addCluster(clusterName1,10015);
         client.addCluster(clusterName1,"localhost");
 
-   /*     Set<String> ids = new HashSet<>();
 
-
-        for (int j=0;j<1;j++) {
-
-            String clusterToUse ;
-            if (j%2==0)
-                clusterToUse = clusterName;
-            else
-                clusterToUse = clusterName1;
-
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader("/Users/mkhanwalkar/test/data/input.txt"));
-
-                String s = null;
-                while ((s = reader.readLine()) != null) {
-                    //  System.out.println(s);
-                    String[] sA = s.split(",");
-
-                    Request request = new Request();
-                    request.setId(sA[0]);
-                    ids.add(sA[0]);
-                    // request.setName(sA[0]);
-                    request.setOperation(DBOperation.AddNode);
-                    request.setDbName("db1");
-                    Response response = client.send(clusterToUse, request);
-                    System.out.println(response);
-
-                    for (int i = 1; i < sA.length; i++) {
-                        //                 Node child = db.createOrGetNode( sA[i]);
-                        Request child = new Request();
-                        child.setId(sA[i]);
-                        ids.add(sA[i]);
-                        // child.setName(sA[i]);
-                        child.setOperation(DBOperation.AddNode);
-                        child.setDbName("db1");
-                        response = client.send(clusterToUse, child);
-                        System.out.println(response);
-
-                        Request relation = new Request();
-                        relation.setId(sA[0]);
-                        relation.setTgtId(sA[i]);
-                        relation.setOperation(DBOperation.AddRelation);
-                        relation.setDbName("db1");
-                        response = client.send(clusterToUse, relation);
-                        System.out.println(response);
-
-                    }
-
-
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-
-         /*   ids.stream().forEach(s->{
-                Request child = new Request();
-                child.setId(s);
-                child.setOperation(DBOperation.DeleteNode);
-                child.setDbName("db1");
-                client.send(clusterToUse, child);
-
-            });*/
         {
 
             MetaRequest meta = new MetaRequest();
@@ -163,12 +70,7 @@ public class ClientTester {
             meta.setMetaData(tableMetaData);
 
 
-         /*   Response response = client.send(clusterName1, meta);
-            System.out.println(response);
 
-            MetaResponse metaResponse = client.query(clusterName1);
-
-            System.out.println(mapper.writeValueAsString(metaResponse));*/
 
             CountRequest countRequest = new CountRequest();
             countRequest.setClusterName("cluster1");
